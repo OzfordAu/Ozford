2ed3375b840189a838f77244299a722907a86290
pushpan999 <getpushpan@gmail.com>
2025-01-15 01:18:19 +0545

    added high school and elicos

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,9 +1,11 @@
 from site_settings.models import SiteSettings
-from courses.models import HigherEducationCoursePage
+from courses.models import HigherEducationCoursePage, HighSchoolCoursePage, ElicosCoursePage
 
 def site_settings(request):
     site_settings = SiteSettings.objects.live().public().first()
     courses = HigherEducationCoursePage.objects.live().public()
+    high_school_courses = HighSchoolCoursePage.objects.live().public()
+    elicos_school_courses = ElicosCoursePage.objects.live().public()
     pg_courses_data = [
     {
         "title": course.course_title,
@@ -24,6 +26,8 @@ def site_settings(request):
             'site_settings': site_settings,
             'global_pg_courses': pg_courses_data,
             'global_ug_courses': ug_courses_data,    
+            'global_high_school_courses': high_school_courses,    
+            'global_elicos_school_courses': elicos_school_courses,    
         }
     else:
         return {}
